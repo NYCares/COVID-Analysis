89bd7aac302a0d7570aac016d2803872fb016694
Karl Ramsay <nytechtiger@gmail.com>
2020-06-07 21:07:07 -0400

    updates to flask

--- app/app.py
+++ app/app.py
@@ -13,9 +13,8 @@ from bson.json_util import dumps
 app = Flask(__name__)
 
 # read json master dataset
-json_data = open(os.path.join(
-    '.', 'static', 'data', 'processed', 'covid_master_analysis.json'))
-print(json_data)
+# json_data = open(os.path.join('.', 'static', 'data', 'processed', 'covid_master_analysis.json'))
+# print(json_data)
 
 # Create connection variable
 conn = 'mongodb://localhost:27017'
@@ -28,8 +27,8 @@ db = dbconn.nycares_db
 
 col = db.covid_master_analysis
 
-master_data = json.load(json_data)
-print(master_data['25']['County'])
+# master_data = json.load(json_data)
+# print(master_data['25']['County'])
 
 # db.covid_analysis.find_one({"county": "Queens"})
 
@@ -54,13 +53,14 @@ def welcome():
 @app.route("/test.html/<county>")
 def Visualization(county):
     master_data = db.covid_master_analysis.find_one({"County": f"{county}"})
-    print(master_data)
-    print(master_data["County"])
-    print(master_data["overall_population"])
-    print(master_data["black_population"])
+
     population = []
     median_income = []
     covid_incidents = []
+    covid_deaths = []
+    fl_pct_county = []
+    fl_pct_state = []
+
     population.append(master_data["asian_population"])
     population.append(master_data["black_population"])
     population.append(master_data["hispanic_latino_population"])
@@ -83,39 +83,18 @@ def Visualization(county):
 
     covid_label = ['covid cases', 'covid deaths']
 
-    # median income array
     covid_incidents.append(master_data["covid_cases"])
-    covid_incidents.append(master_data["covid_deaths"])
+    covid_deaths.append(master_data["covid_deaths"])
 
-    # covid cases and deaths
+    print(covid_incidents)
+    print(covid_deaths)
+    lunch_data = db.free_and_reduced_lunch.find_one({"region": f"{county}"})
+    fl_pct_county.append(lunch_data["percent of the"])
+    state_data = db.free_and_reduced_lunch.find_one(
+        {"region": "New York State"})
+    fl_pct_state.append(state_data["percent of state"])
 
-    # for item in master_data:
-    #     # get the data and the labels for each of the five (5) charts
-    #     pop_label = ['asian', 'black', 'hispanic', 'native american',
-    #                  'other, ', 'pacific islander', 'white']
-    #     population = [master_data[item][6],
-    #                   master_data[item][4],
-    #                   master_data[item][5]]
-    # print(master_data[item])
-    # data1.append(item[])
- # get the labels an
-
-    return render_template('test.html', population=population, pop_label=pop_label, covid_label=covid_label, covid_incidents=covid_incidents, median_income=median_income)
-
-# @app.route("/results.html")
-# def Visualization():
-#     return render_template('results.html', data=median_income)
-
-
-# @app.route("/il-data.html")
-# def ViewILData():
-#     jobs = db.jobs.find().limit(50)
-#     return render_template('il-data.html', jobs=jobs)
-
-
-# @app.route("/team.html")
-# def About():
-#     return render_template('team.html')
+    return render_template('test.html', population=population, pop_label=pop_label, covid_label=covid_label, covid_incidents=covid_incidents, covid_deaths=covid_deaths,  median_income=median_income, fl_pct_county=fl_pct_county, fl_pct_state=fl_pct_state)
 
 
 if __name__ == '__main__':
--- app/static/js/logic.js
+++ app/static/js/logic.js
@@ -1,108 +1,120 @@
-var map = L.map("map", {
+var map = L.map('map', {
   center: [42.75, -75.5],
-  zoom: 7
+  zoom: 7,
 });
-  
+
 // Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: 'mapbox/light-v9',
-  tileSize: 512,
-  zoomOffset: -1,
-  accessToken: API_KEY
-}).addTo(map);
-  
+L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v9',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY,
+  }
+).addTo(map);
+
 // Link to local geojson
-var countyLink = "../static/data/ny_color.geojson";
+var countyLink = '../static/data/ny_color.geojson';
 
 function getColor(d) {
-  return d > 60000 ? '#800026' :
-  d > 50000  ? '#BD0026' :
-  d > 20000  ? '#E31A1C' :
-  d > 10000  ? '#FC4E2A' :
-  d > 5000   ? '#FD8D3C' :
-  d > 2000   ? '#FEB24C' :
-  d > 1000   ? '#FED976' :
-             '#FFEDA0';
+  return d > 60000
+    ? '#800026'
+    : d > 50000
+    ? '#BD0026'
+    : d > 20000
+    ? '#E31A1C'
+    : d > 10000
+    ? '#FC4E2A'
+    : d > 5000
+    ? '#FD8D3C'
+    : d > 2000
+    ? '#FEB24C'
+    : d > 1000
+    ? '#FED976'
+    : '#FFEDA0';
 }
 
 function style(feature) {
   return {
-      fillColor: getColor(feature.properties.percent),
-      weight: 2,
-      opacity: 1,
-      color: 'white',
-      dashArray: '3',
-      fillOpacity: 0.7
+    fillColor: getColor(feature.properties.percent),
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7,
   };
 }
 
 // Grabbing our GeoJSON data..
-d3.json(countyLink, function(data) {
+d3.json(countyLink, function (data) {
   // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(data,{
+  L.geoJson(data, {
     style: style,
 
     // called on each feature
-    onEachFeature: function(feature, layer) {
+    onEachFeature: function (feature, layer) {
       // identify county name
-      label = feature.properties.name
+      label = feature.properties.name;
 
-      layer.bindPopup("<h3>" + label + "</h3><hr>Click for more data")
+      layer.bindPopup('<h3>' + label + '</h3><hr>Click for more data');
 
       // darken color on hover
       layer.on({
-        mouseover: function(event) {
-          layer = event.target
+        mouseover: function (event) {
+          layer = event.target;
           layer.setStyle({
             fillOpacity: 1,
             weight: 5,
             color: '#666',
-            dashArray: ''
-          })
+            dashArray: '',
+          });
           if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
             layer.bringToFront();
           }
-          info.update(layer.feature.properties)
-          layer.openPopup()
-          
+          info.update(layer.feature.properties);
+          layer.openPopup();
         },
-        mouseout: function(event) {
-          layer = event.target
+        mouseout: function (event) {
+          layer = event.target;
           layer.setStyle({
             fillOpacity: 0.7,
             weight: 2,
             opacity: 1,
             color: 'white',
-            dashArray: '3'
-          })
-          info.update()
-          layer.closePopup()
+            dashArray: '3',
+          });
+          info.update();
+          layer.closePopup();
         },
-        click: function(event) {
-          layer = event.target
-          county = layer.feature.properties.name
-          console.log(county)
-          window.location = ("../templates/sample.html")
-        }
-      })
-    }
+        click: function (event) {
+          layer = event.target;
+          county = layer.feature.properties.name;
+          console.log(county);
+          window.location.href = '../templates/test.html/' + county;
+        },
+      });
+    },
   }).addTo(map);
 
-  var legend = L.control({position: 'bottomright'});
+  var legend = L.control({ position: 'bottomright' });
 
   legend.onAdd = function (map) {
-
     var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 1000, 2000, 5000, 10000, 20000, 50000, 60000],
-        labels = [];
+      grades = [0, 1000, 2000, 5000, 10000, 20000, 50000, 60000],
+      labels = [];
 
     // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      div.innerHTML +=
+        '<i style="background:' +
+        getColor(grades[i] + 1) +
+        '"></i> ' +
+        grades[i] +
+        (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
 
     return div;
@@ -119,12 +131,16 @@ d3.json(countyLink, function(data) {
 
   // method that we will use to update the control based on feature properties passed
   info.update = function (props) {
-    this._div.innerHTML = '<h4>NY Covid Cases as of May 22</h4>' +  (props ?
-        '<b>' + props.name + '</b><br />' + props.percent + ' people with COVID-19'
+    this._div.innerHTML =
+      '<h4>NY Covid Cases as of May 22</h4>' +
+      (props
+        ? '<b>' +
+          props.name +
+          '</b><br />' +
+          props.percent +
+          ' people with COVID-19'
         : 'Hover over a county');
   };
 
   info.addTo(map);
-
-
 });
